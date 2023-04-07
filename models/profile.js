018ffb62a68eaa38d42fddb55ff798bc951e194b
Yong Park <pyongho0306@gmail.com>
2023-04-06 23:59:24 -0700

    Add default avatar photo

--- models/profile.js
+++ models/profile.js
@@ -5,7 +5,11 @@ const Schema = mongoose.Schema;
 const profileSchema = new Schema(
   {
     name: String,
-    photo: String,
+    photo: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dmbhhnc2j/image/upload/v1675870453/AdobeStock_349497933_xq1qwl.jpg",
+    },
     posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   },
   {
